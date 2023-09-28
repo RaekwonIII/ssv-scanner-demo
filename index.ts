f4057d318b5911a279026cdfb11c5118717d2e48
Massimo Luraschi <massimo.lura@gmail.com>
2023-09-28 14:46:47 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log("Hello via Bun!");
+
+// npm i @web3modal/react @web3modal/ethereum
+// npm i wagmi web3modal/react @web3modal/ethereum
+// https://api.studio.thegraph.com/query/53804/ssv-subgraph/version/latest
